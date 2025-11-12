a6f438fa3820dfbdfc2a3b70b276a7486a1babd9
Victor Rodriguez <rodicecco@outlook.com>
2025-11-11 20:10:53 -0500

    initial commit

---
+++ backend/content/secret.py
@@ -0,0 +1,14 @@
+key_chain = {'EOD': ' 62cb6d4b152f86.30923161', 
+             'FRED': '87ffc2a749498847e26809a500f9036a'}
+
+
+
+database = { 
+            'GOOG':{ #set up for google cloud database
+            'host':'34.60.50.195',
+            'database':'datadb',
+            'user':'postgres',
+            'password':'Vic24278175.', 
+            'port':'5432'
+                    }
+            }
